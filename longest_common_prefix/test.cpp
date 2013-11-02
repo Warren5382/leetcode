89e364ee5eef6bffb68c5fd139511570e3580bb3
wangzhenMac <thueew@gmail.com>
2013-11-02 15:41:56 +0800

    divide

--- longest_common_prefix/test.cpp
+++ longest_common_prefix/test.cpp
@@ -24,3 +24,36 @@ class Solution {
 			return result;
 		}
 };
+
+class Solution {
+	public:
+		bool isEqual(vector<string> &strs, int index, int start, int end) {
+			if(end < start) return true;
+			if(end == start) {
+				if(strs[start].size() > index) return true;
+				else return false;
+			}
+			int mid = (start + end) / 2;
+			bool lEqual = isEqual(strs, index, start, mid);
+			bool rEqual = isEqual(strs, index, mid + 1, end);
+			if(!lEqual || !rEqual) return false;
+			if(index > strs[start].size()) return false;
+			if(index > strs[end].size()) return false;
+			if(strs[start][index] != strs[end][index]) return false;
+			return true;
+		}
+		string longestCommonPrefix(vector<string> &strs) {
+			// Note: The Solution object is instantiated only once and is reused by each test case.
+			string res;
+			size_t vSize = strs.size();
+			if(0 == vSize) return res;
+			if(1 == vSize) return strs[0];
+
+			int len = 0;
+			while(isEqual(strs, len, 0, vSize - 1)) {
+				len ++;
+			}
+			res = strs[0].substr(0, len);
+			return res;
+		}
+};
--- two_sum/test.cpp
+++ two_sum/test.cpp
@@ -22,3 +22,25 @@ class Solution {
 			return ret;
 		}
 };
+
+class Solution {
+	public:
+		vector<int> twoSum(vector<int> &numbers, int target) {
+			// IMPORTANT: Please reset any member data you declared, as
+			// the same Solution instance will be reused for each test case.
+			unordered_map<int, int> index_map;
+			unordered_map<int, int>::iterator it;
+			vector<int> result;
+			for (int i=0; i<numbers.size(); ++i) {
+				it = index_map.find(target-numbers[i]);
+				if (it != index_map.end()) {
+					result[0] = it->second;
+					result[1] = i+1;
+					return result;
+				} else {
+					index_map[numbers[i]] = i+1;
+				}
+			}
+			return result;
+		}
+};
